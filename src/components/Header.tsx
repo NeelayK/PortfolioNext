e26026a554f51e9cd66e7581eb514f12e69af030
Neelay <75205019+NeelayK@users.noreply.github.com>
2025-06-25 16:52:38 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,11 +19,11 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed top-0 w-full bg-light-header dark:bg-dark-header backdrop-blur-sm z-50 transition-transform duration-300 ${
+      className={`fixed top-0 w-screen bg-light-header dark:bg-dark-header backdrop-blur-sm z-50 transition-transform duration-300 ${
         hidden ? '-translate-y-full' : 'translate-y-0'
       }`}
     >
-      <div className="w-full max-w-screen-xl mx-auto flex justify-between items-center py-8 px-4 sm:px-6 md:px-12">
+      <div className="max-w-8xl mx-auto flex justify-between items-center py-8 px-6 md:px-12">
         <a
           href="#hero"
           className="text-xl font-bold text-default dark:text-parchment cursor-none"
